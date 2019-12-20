d4f992d7a3b26a74156b71aa1cf5e704962431b8
flamingotiger <hhbhong@gmail.com>
2019-12-20 18:01:11 +0900

    Add redux configure

--- src/components/VideoPlayer/index.tsx
+++ src/components/VideoPlayer/index.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Card } from "react-bootstrap";
 
 const VideoPlayer: React.FC = () => {
-    return <Card>VideoPlayer</Card>
+    return <Card>
+        <video src='https://www.youtube.com/watch?v=RDQGPs7StNA'/>
+    </Card>
 };
 
 export default VideoPlayer;
---
+++ src/store/reducers/index.ts
@@ -0,0 +1,8 @@
+import { combineReducers } from "redux";
+
+const RootReducer = combineReducers({
+
+});
+
+export type RootState = ReturnType<typeof RootReducer>
+export default RootReducer;
