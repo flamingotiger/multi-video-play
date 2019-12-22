8d896a7c7d5f5c639d3c2b32434d4965b5f2153d
honghakbum <hhbhong@gmail.com>
2019-12-22 22:17:04 +0900

    Updates

---
+++ precache-manifest.471bd27422844276a136880e9490d3c2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80cb89c479b395c5c18ca9a6cc81911b",
+    "url": "/multi-video-play/index.html"
+  },
+  {
+    "revision": "163eedb3fd5fd00e7408",
+    "url": "/multi-video-play/static/css/main.d77e865f.chunk.css"
+  },
+  {
+    "revision": "447a334fe8bedefab8d9",
+    "url": "/multi-video-play/static/js/2.d2d2861c.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "/multi-video-play/static/js/2.d2d2861c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "163eedb3fd5fd00e7408",
+    "url": "/multi-video-play/static/js/main.071819d9.chunk.js"
+  },
+  {
+    "revision": "f2de3c3269eaefabffdf",
+    "url": "/multi-video-play/static/js/runtime-main.3e9e8af0.js"
+  },
+  {
+    "revision": "404cf0dbf350e6ab7f66415b6e101d8a",
+    "url": "/multi-video-play/static/media/splash.404cf0db.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/multi-video-play/precache-manifest.31a0deaaca32ef441ceee329534bcbed.js"
+  "/multi-video-play/precache-manifest.471bd27422844276a136880e9490d3c2.js"
 );
 
 self.addEventListener('message', (event) => {
